e7c35a40b794de1f9af0c781fd0b9dd4221f9796
James Lisenby <jameslisenby@Jamess-MacBook-Pro.local>
2022-11-23 22:27:00 -0900

    Adjusted navbar and mobile sizing

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa'
 import Logo from '../assets/logo.png'
 
 const Navbar = () => {
+
+const [nav, setNav] = useState(false)
+const handlClick = () => setNav(!nav) 
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#005] text-gray-300'>
         <div>
-            <img src={Logo} alt="Logo Imagenp" style={{width: '50px'}}/>
+            <img src={Logo} alt="Logo Image" style={{width: '50px'}}/>
         </div>
 
 
@@ -23,20 +27,20 @@ const Navbar = () => {
         
 
         {/* Hamburger */}
-        <div className='md:hidden z-10'>
+        <div onClick={handlClick} className='md:hidden z-10'>
             <FaBars />
         </div>
 
         {/* Mobile Menu*/}
-        <div className='hidden'>
-        <ul className='absolute top-0 left-0 w-full h-screen bg-[#005] flex flex-col justify-center items-center'>
+        
+        <ul className={!nav ? 'hidden' :  'absolute top-0 left-0 w-full h-screen bg-[#005] flex flex-col justify-center items-center' } >
                 <li className='py-6 text-4xl'>Home</li>
                 <li className='py-6 text-4xl'>About</li>
                 <li className='py-6 text-4xl'>Skills</li>
                 <li className='py-6 text-4xl'>Work</li>
                 <li className='py-6 text-4xl'>Contact</li>
             </ul>
-        </div>
+    
 
         {/* Social Icons */}
         <div className='hidden'></div>
