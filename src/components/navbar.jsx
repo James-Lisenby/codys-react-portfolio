42db7df0e3b74d7ec23ea5739059c77b789896fe
James Lisenby <jameslisenby@Jamess-MacBook-Pro.local>
2022-11-23 16:25:17 -0900

    Created more on the navbar componet

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import Logo from '../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#005]'></div>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#005] text-gray-300'>
+        <div>
+            <img src={Logo} alt="Logo Imagenp" style={{width: '50px'}}/>
+        </div>
+
+
+
+        {/* Menu */}
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Work</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+
+        {/* Hamburger */}
+        <div>
+            <FaBars />
+        </div>
+
+        {/* Mobile */}
+        <div></div>
+
+        {/* Social Icons */}
+        <div></div>
+
+
+    </div>
   )
 }
 
