b6ec5a940e586d3287b721b7e71e4ffe9d43111f
James Lisenby <jameslisenby@Jamess-MacBook-Pro.local>
2022-11-23 22:37:28 -0900

    Psuedo code describing some of the code for the navbar component.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -27,13 +27,15 @@ const handlClick = () => setNav(!nav)
         
 
         {/* Hamburger */}
+        {/* The styling below makes it so the hamburger is only visible when the screen is 760px or less. */}
         <div onClick={handlClick} className='md:hidden z-10'>
             <FaBars />
         </div>
 
         {/* Mobile Menu*/}
+        {/* The styling below is for the menu that appears on mobile devices. */}
         
-        <ul className={!nav ? 'hidden' :  'absolute top-0 left-0 w-full h-screen bg-[#005] flex flex-col justify-center items-center' } >
+        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#005] flex flex-col justify-center items-center' } >
                 <li className='py-6 text-4xl'>Home</li>
                 <li className='py-6 text-4xl'>About</li>
                 <li className='py-6 text-4xl'>Skills</li>
