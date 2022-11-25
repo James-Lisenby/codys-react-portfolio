27cb523eb58ab3ba325a5863b7e9884dc03d597d
James Lisenby <jameslisenby@Jamess-MacBook-Pro.local>
2022-11-25 11:44:06 -0900

    Styling the skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,17 +11,17 @@ import GitHub from '../assets/github.png'
 
 const Skills = () => {
   return (
-    <div name='skills'>
+    <div name='skills' className='bg-[#005] text-gray-500'>
         {/* Container */}
+<div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
 <div>
-<div>
-    <p>Experience</p>
-    <p>I have some experience with the following technologies!</p>
+    <p className='text-4xl inline border-b-4 border-green-300'>Experience</p>
+    <p className='py-4'>I have some experience with the following technologies!</p>
 </div>
 
-<div>
+<div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
   <div>
-    <img src={HTMl} alt="HTML icon" />
+    <img className='width-20 mx-auto' src={HTML} alt="HTML icon" />
     <p>HTML</p>
   </div>
 </div>
