f9e1d599a7210273ab8a51e4a4e9bffb4477e15f
James Lisenby <jameslisenby@Jamess-MacBook-Pro.local>
2022-11-25 14:13:12 -0900

    created a contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Contact = () => {
+  return (
+    <div name='contact' className='w-full h-screen bg-[#005] flex justify-center items-center p-4'>
+        <form action="" className='flex flex-col max-w-[600px] w-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-green-300 text-gray-500'>Contact</p>
+            </div>
+        </form>
+
+    </div>
+  )
+}
