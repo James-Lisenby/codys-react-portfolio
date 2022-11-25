d673f7bd1c304e577cdb20157758505edc16eee6
James Lisenby <jameslisenby@Jamess-MacBook-Pro.local>
2022-11-25 10:40:27 -0900

    finished home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         <h1 className='text-gray-500 text-4xl'>Cody Lisenby</h1>
         <h2 className='text-gray-500 text-4xl'>I am an aspiring full stack web developer!</h2>
         <div>
-            <button className='text-white border-2 px-6 py-3 my-2 flex items-center'>
+            <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-green-300 hover:border-color-green-300'>
                 View Work <HiArrowNarrowRight />
             </button>
         </div>
