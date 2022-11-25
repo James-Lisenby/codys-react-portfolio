8c6cbd365dc5b1cdf159379405e856eda0d8c9b2
James Lisenby <jameslisenby@Jamess-MacBook-Pro.local>
2022-11-25 13:00:08 -0900

    Building work component and styling

---
+++ src/components/Work.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-500 bg-[#005]'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div classname='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 text-gray-500 border-green-300'>Work</p>
+                <p className='py-6'>Come take a look at some of my work!</p>
+            </div>
+
+<div className='grid sm:grid-col-2 md:grid-col-3 gap-4'>
+    <div>
+        <div>
+         <span>
+
+         </span>
+         <div>
+            <a href="/">
+                <button></button>
+            </a>
+            <a href="/">
+                <button></button>
+            </a>
+         </div>
+        </div>
+    </div>
+</div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Work
