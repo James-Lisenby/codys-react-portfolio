afd499a35922f5fb4ad0ff518aafc8d285605257
James Lisenby <jameslisenby@Jamess-MacBook-Pro.local>
2022-11-25 10:24:12 -0900

    Building the Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,14 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#005]'>
+    <div name='home' className='w-full h-screen bg-[#005]'>
+
+    {/* Container */}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full '>
+        <p>Hello! My name is </p>
+        <h1>Cody Lisenby</h1>
+        <h2>I am an aspiring full stack web developer!</h2>
+        </div>
 
     </div>
   )
