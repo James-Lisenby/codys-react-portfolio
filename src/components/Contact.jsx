2e535b7a586daa62b3dd63a54e29e294b240de05
James Lisenby <jameslisenby@Jamess-MacBook-Pro.local>
2022-11-25 14:35:38 -0900

    styling the contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,12 @@ export const Contact = () => {
         <form action="" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-green-300 text-gray-500'>Contact</p>
+                <p className='text-gray-500 py-4'>Submit the form below or email me at james123lisenby@hotmail.com</p>
             </div>
+            <input className='bg-green-300 p-2' type="text" placeholder='Name' name='name'/>
+            <input className='bg-green-300 my-4 p-2' type="email" placeholder='Email' name='email'/>
+            <textarea className='bg-green-300 p-2' name="message" rows="10" placeholder='Message'></textarea>
+            <button className='text-white border-2 hover:bg-green-300 hover:border-green-300 px-4 py-3 my-8 mx-auto flex items-center'>Lets do work!</button>
         </form>
 
     </div>
