96c5f3d20068219f48a4bb87c832e7332a36c558
James Lisenby <jameslisenby@Jamess-MacBook-Pro.local>
2022-11-25 14:51:04 -0900

    added react-scroll and getform.io

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#005] flex justify-center items-center p-4'>
-        <form action="" className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action="https://getform.io/f/b3da60b4-00a5-4ed4-b7d6-beee526a7c06" className='flex flex-col max-w-[600px] w-full'>
             <div className='p-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-green-300 text-gray-500'>Contact</p>
                 <p className='text-gray-500 py-4'>Submit the form below or email me at james123lisenby@hotmail.com</p>
